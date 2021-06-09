2605e2f7236d0407fad1913ded8f1a3f57382702
miyabi-satoh <miyabi.satoh@gmail.com>
2021-06-09 12:44:54 +0900

    archivesルート追加

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r'units', views.UnitViewSet)
 router.register(r'prints', views.PrintViewSet)
+router.register(r'archives', views.ArchiveViewSet)
 
 urlpatterns = [
     path('hello/', views.HelloView.as_view()),
