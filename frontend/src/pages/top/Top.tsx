36f421c672aa3f6a066867cf4443301133277c54
miyabi-satoh <miyabi.satoh@gmail.com>
2021-06-10 12:53:11 +0900

    本文追加

--- frontend/src/pages/top/Top.tsx
+++ frontend/src/pages/top/Top.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@material-ui/core";
+import { RouterLink } from "components";
 
 function Top() {
   return (
@@ -10,6 +11,19 @@ function Top() {
       </Box>
       <Box m={2}>
         <Typography variant="body1">プリント保管庫的なアレです。</Typography>
+      </Box>
+      <Box m={2}>
+        <Typography variant="body1">
+          <RouterLink to="/prints">プリントセット</RouterLink>では
+          プリント内容を新たに定義したり、
+          作成済みのプリント定義から新しいプリントを印刷したりできます。
+        </Typography>
+      </Box>
+      <Box m={2}>
+        <Typography variant="body1">
+          <RouterLink to="/archives">アーカイブ</RouterLink>では
+          作成済みのプリントを再印刷できます。
+        </Typography>
       </Box>
     </>
   );
