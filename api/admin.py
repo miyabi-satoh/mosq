87b5e0b1597b8531ef2c0298825035e3115cac49
miyabi-satoh <miyabi.satoh@gmail.com>
2021-06-11 12:54:01 +0900

    チェンジリストをカスタマイズ

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,48 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from . import models
 
 
-admin.site.register(models.Grade)
-admin.site.register(models.Unit)
-admin.site.register(models.Question)
-admin.site.register(models.PrintHead)
-admin.site.register(models.PrintDetail)
-admin.site.register(models.Archive)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('grade_text', 'grade_code', 'id')
+
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('unit_text', 'unit_code', 'grade', 'id')
+    list_filter = ['grade']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'answer_text',
+                    'unit', 'source_text', 'url_link', 'id')
+    list_filter = ['unit']
+
+    @admin.display(description='参照')
+    def url_link(self, obj):
+        if obj.url_text:
+            return format_html(
+                '<a href="{}" target="_blank">Open</a>',
+                obj.url_text)
+        else:
+            return ''
+
+
+class PrintHeadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'total_questions', 'id')
+
+
+class PrintDetailAdmin(admin.ModelAdmin):
+    list_display = ('unit', 'quantity', 'printhead', 'id')
+    list_filter = ['printhead']
+
+
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'file', 'id')
+
+
+admin.site.register(models.Grade, GradeAdmin)
+admin.site.register(models.Unit, UnitAdmin)
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.PrintHead, PrintHeadAdmin)
+admin.site.register(models.PrintDetail, PrintDetailAdmin)
+admin.site.register(models.Archive, ArchiveAdmin)
